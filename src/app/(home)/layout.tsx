5e4477f9f1e65f888e0e33084ed81c2bfe3c1a5a
Sajid Ahmad <sajid01ahmad@gmail.com>
2025-07-20 13:33:28 +0530

    footer and bg-color chnage in home-layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./footer";
 import { Navbar } from "./Navbar";
 
 interface Props {
@@ -8,7 +9,8 @@ const layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      {children}
+      <div className="flex-1 bg-[#F4F4F0]">{children}</div>
+      <Footer />
     </div>
   );
 };
