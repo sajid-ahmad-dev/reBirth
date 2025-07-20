afad6b8ebf8042999205a1cb6f9070b6d4eb785e
Sajid Ahmad <sajid01ahmad@gmail.com>
2025-07-20 13:03:14 +0530

    added the responsive navBar

--- src/app/(home)/Navbar.tsx
+++ src/app/(home)/Navbar.tsx
@@ -4,6 +4,9 @@ import Link from "next/link";
 import { Poppins } from "next/font/google";
 import { Button } from "@/components/ui/button";
 import { usePathname } from "next/navigation";
+import { NavbarSidebar } from "./navbar-sideBar";
+import { useState } from "react";
+import { MenuIcon } from "lucide-react";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["700"],
@@ -39,6 +42,7 @@ const navbarItem = [
 
 export const Navbar = () => {
   const pathname = usePathname();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
     <nav className="h-20 flex border-b justify-between font-medium bg-white">
       <Link href="/" className="pl-6 flex items-center">
@@ -46,6 +50,11 @@ export const Navbar = () => {
           reBirth
         </span>
       </Link>
+      <NavbarSidebar
+        items={navbarItem}
+        open={isSidebarOpen}
+        onOpenChange={setIsSidebarOpen}
+      />
 
       <div className="items-center gap-4 hidden lg:flex">
         {navbarItem.map((item) => (
@@ -58,7 +67,7 @@ export const Navbar = () => {
           </NavbarItem>
         ))}
       </div>
-      <div>
+      <div className="hidden lg:flex">
         <Button
           asChild
           variant="secondary"
@@ -72,6 +81,15 @@ export const Navbar = () => {
           className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 transition-colors text-lg"
         >
           <Link href="/sign-up">Start selling</Link>
+        </Button>
+      </div>
+      <div className="flex lg:hidden items-center justify-center">
+        <Button
+          variant="ghost"
+          className="size-12 border-transparent bg-white"
+          onClick={() => setIsSidebarOpen(true)}
+        >
+          <MenuIcon />
         </Button>
       </div>
     </nav>
